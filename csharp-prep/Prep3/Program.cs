40dc2bd844cb7c9909c3e8a06e7d490c6f6c2b8e
I-FIEVRE <fievre.isabelle@gmail.com>
2023-01-10 15:21:04 +0100

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string choice = "lost";
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //Console.Write("What is the magic number? ");
+        //string userInput = Console.ReadLine();
+        //int magicNumber = int.Parse(userInput);
+
+        string input;
+        do
+        {
+             Console.Write("What is your guess? ");
+             input = Console.ReadLine();
+             int userGuess = int.Parse(input);
+            if (userGuess > magicNumber) 
+            {
+                Console.WriteLine("Lower");
+            } 
+            else if (userGuess < magicNumber) 
+            {
+                Console.WriteLine("Higher");
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+                choice = "win";
+            }
+        } while (choice == "lost");
+       
     }
-}
+}    
