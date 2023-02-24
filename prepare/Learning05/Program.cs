30e78decc404c7965371d65696288937bc1c4236
I-FIEVRE <fievre.isabelle@gmail.com>
2023-02-24 18:59:27 +0100

    Activity 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        
+        Square sq = new Square("red", 3.2);
+        Rectangle rec = new Rectangle("green", 3.2, 2.5);
+        Circle cir = new Circle("blue", 4.5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(sq);
+        shapes.Add(rec);
+        shapes.Add(cir);
+
+        foreach(Shape sh in shapes)
+        {
+            string color = sh.GetColor();
+            double area = sh.GetArea();
+            Console.WriteLine($"The {color} shape of paper has an area of {area}.");
+        }
+
+        /*string sqColor = sq.GetColor();
+        double sqArea = sq.GetArea();
+        Console.WriteLine($"{sqColor} {sqArea}");
+
+        string recColor = rec.GetColor();
+        double recArea = rec.GetArea();
+        Console.WriteLine($"{recColor} {recArea}");
+
+        string cirColor = cir.GetColor();
+        double cirArea = cir.GetArea();
+        Console.WriteLine($"{cirColor} {cirArea}");*/
     }
 }
